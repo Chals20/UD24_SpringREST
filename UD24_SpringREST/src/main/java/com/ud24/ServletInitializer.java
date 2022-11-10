9b372cbbc5208e02edf2863aeda0555d5850eacb
Chals <csanchezbasket@gmail.com>
2022-11-10 09:52:58 +0100

    Cambio package

--- UD24_SpringREST/src/main/java/com/ud24/ServletInitializer.java
+++ UD24_SpringREST/src/main/java/com/ud24/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.ud24;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- UD24_SpringREST/src/main/java/com/ud24/Ud24SpringRestApplication.java
+++ UD24_SpringREST/src/main/java/com/ud24/Ud24SpringRestApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.ud24;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
